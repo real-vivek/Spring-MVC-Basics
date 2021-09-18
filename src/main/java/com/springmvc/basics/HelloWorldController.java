4127b862fc03accc20bb21aa6f4954eb090ffe45
Vivek Yadav <vivek.mahadev.yadav@gmail.com>
2021-09-18 14:11:15 +0530

    Example of ways to call view template

--- src/main/java/com/springmvc/basics/HelloWorldController.java
+++ src/main/java/com/springmvc/basics/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.springmvc.basics;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloWorldController {
@@ -10,4 +11,18 @@ public class HelloWorldController {
 	public String showHomePage() {
 		return "home";
 	}
+
+	@RequestMapping("/v1")
+	public ModelAndView showFormPage() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("home");
+		return modelAndView;
+	}
+
+	@RequestMapping("/v2")
+	public ModelAndView showFormPage(ModelAndView modelAndView) {
+		modelAndView.setViewName("home");
+		return modelAndView;
+	}
+
 }
