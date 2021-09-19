2246abf567a4f07c98755bb395bc2ba2d300cf7b
Vivek Yadav <vivek.mahadev.yadav@gmail.com>
2021-09-19 18:54:32 +0530

    Example of how to forward and redirect request in Spring MVC

--- src/main/java/com/springmvc/basics/HelloWorldController.java
+++ src/main/java/com/springmvc/basics/HelloWorldController.java
@@ -19,10 +19,22 @@ public class HelloWorldController {
 		return modelAndView;
 	}
 
+	// Spring will create ModelAndView Object and injects it in method params
 	@RequestMapping("/v2")
 	public ModelAndView showFormPage(ModelAndView modelAndView) {
 		modelAndView.setViewName("home");
 		return modelAndView;
 	}
-
+	
+	// Example of forwarding request(to URL in same app)
+	@RequestMapping("/v1/forward")
+	public String showFormPageForwardReq() {
+		return "forward:/";
+	}
+	
+	// Example of redirecting request(to URL in different app)
+	@RequestMapping("/v1/redirect")
+	public String showFormPageRedirectReq() {
+		return "redirect:https://www.google.com/";
+	}
 }
