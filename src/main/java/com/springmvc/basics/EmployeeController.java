3a5a2ac853e70060b9c3e6e0a47cfe0b899774e9
Vivek Yadav <vivek.mahadev.yadav@gmail.com>
2021-09-22 18:25:19 +0530

    Example of handling pre-processing of web request using InitBinder

--- src/main/java/com/springmvc/basics/EmployeeController.java
+++ src/main/java/com/springmvc/basics/EmployeeController.java
@@ -2,9 +2,12 @@ package com.springmvc.basics;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class EmployeeController {
 
+	@InitBinder
+	public void initBInder(WebDataBinder webDataBinder) {
+		// StringTrimmerEditor will trim leading and trailing whitespaces
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);// true value means trim to null
+		webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	
+	
 	@RequestMapping("/employee")
 	public String createEmployee(Model model) {
 		// This employee bean will be populated automatically in employee.jsp page because of spring form tag
